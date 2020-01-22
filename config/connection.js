d50e11cf26e352834b155b211949c6c0c84cc5b6
Aidan Willett <Gamer9310@gmail.com>
2020-01-21 18:10:25 -0800

    added shark stuff

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL){
+  console.log("going to the internet.")
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  console.log ("going local");
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
@@ -15,5 +29,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
 
 module.exports = connection;
